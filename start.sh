029ecba4069f27bd4f347c3cd1db86af7d1ed4eb
Paul Walsh <paulgwalsh@gmail.com>
2019-08-02 13:25:32 +0100

    add new env variables for databases to enable testing

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#Update db_host in cgrates.json
+#Update db values in cgrates.json
 sed -i 's/MONGO_HOST/'"$MONGO_HOST"'/g' /etc/cgrates/cgrates.json
+sed -i 's/MONGO_DATADB/'"$MONGO_DATADB"'/g' /etc/cgrates/cgrates.json
+sed -i 's/MONGO_STORDB/'"$MONGO_STORDB"'/g' /etc/cgrates/cgrates.json
 
 #cat the log file to screen
 #cat /etc/cgrates/cgrates.json
