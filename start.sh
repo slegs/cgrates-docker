785ed09b029cd75944948388481d6db6bd3edbe2
Paul Walsh <paulgwalsh@gmail.com>
2019-08-03 01:01:39 +0100

    changed to stateful set logic

--- start.sh
+++ start.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+#Get ordinal of statefulset pod and add a - in front. If no number then just leave blank
+ORDINAL=${CGRATES_NAME##*-}
+
+re='^[0-9]+$'
+if  ! [[ $ORDINAL =~ $re ]] ; then
+        ORDINAL=""
+else
+        ORDINAL="-$ORDINAL"
+fi
+
 #Update db values in cgrates.json
 sed -i 's/MONGO_HOST/'"$MONGO_HOST"'/g' /etc/cgrates/cgrates.json
 sed -i 's/MONGO_DATADB/'"$MONGO_DATADB"'/g' /etc/cgrates/cgrates.json
 sed -i 's/MONGO_STORDB/'"$MONGO_STORDB"'/g' /etc/cgrates/cgrates.json
+sed -i 's/CGRATES_KAMAILIO_ENABLED/'"$CGRATES_KAMAILIO_ENABLED"'/g' /etc/cgrates/cgrates.json
+sed -i 's/CGRATES_KAMAILIO_ADDRESS/'"$KAMAILIO_NAME""$ORDINAL""$KAMAILIO_SUFFIX"':'"$KAMAILIO_PORT"'/g' /etc/cgrates/cgrates.json
 
 #cat the log file to screen
 #cat /etc/cgrates/cgrates.json
